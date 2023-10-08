cad996fa8058d0c1e7d220e9d5a7cff02e9216d4
janhavi310 <147117780+janhavi310@users.noreply.github.com>
2023-10-08 17:48:20 +0530

    Add files via upload

---
+++ 6Oct2023.py
@@ -0,0 +1,20 @@
+# Task 1: Take a input from a user and print the table
+# How to use the print with formatting f
+# print(f''), How can print the same?
+# '''n = 2 & print
+# 2 x 1 = 2 .. 2 x 10 = 20
+
+
+input = (input("Enter the number"))
+n = 2
+print(f"2 * 1 = {n* 1}")
+print(f"2 * 2 = {n* 2}")
+print(f"2 * 3 = {n* 3}")
+print(f"2 * 4 = {n* 4}")
+print(f"2 * 5 = {n* 5}")
+print(f"2 * 6 = {n* 6}")
+print(f"2 * 7 = {n* 7}")
+print(f"2 * 8 = {n* 8}")
+print(f"2 * 9 = {n* 9}")
+print(f"2 * 10 = {n* 10}")
+
