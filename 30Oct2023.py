7367e1d4e2e9f74dc7f991bcb66f69f25060c64f
jboxey <42467141+jboxey@users.noreply.github.com>
2023-11-01 07:03:22 +0530

    Add files via upload

---
+++ 30Oct2023.py
@@ -0,0 +1,16 @@
+#Find the maximum and minimum elements in a tuple.
+
+my_tuple = (15, 8, 25, 36, 42, 10)
+print(max(my_tuple))
+
+#Find the intersection and union of two sets.
+
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {3, 4, 5, 6, 7}
+
+intersection = set1.intersection(set2)
+print(intersection)
+
+union = set1.union(set2)
+print(union)
